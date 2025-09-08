96e96a761085ef9158fc4f21a3428963d6d9a6dd
Prabhat Mishra <prabhat1284@gmail.com>
2025-09-08 15:11:11 +0530

    email require validation

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -157,5 +157,24 @@ describe('POST /auth/register', () => {
         })
     })
 
-    describe('Given all fields', () => {})
+    describe('Fields are missing', () => {
+        it('should return 400 status code if email field is missing', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'Prabhat',
+                lastName: 'Mishra',
+                email: '',
+                password: 'password',
+            }
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            expect(response.statusCode).toBe(400)
+            const userRepository = connection.getRepository(User)
+            const users = await userRepository.find()
+            expect(users).toHaveLength(0)
+        })
+    })
 })
