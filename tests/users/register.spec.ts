b75d3b66f4a41eb2f929f7ba43bfd8bde1651941
Prabhat Mishra <prabhat1284@gmail.com>
2025-09-08 14:21:01 +0530

    Add test for unique email

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -3,6 +3,7 @@ import app from '../../src/app'
 import request from 'supertest'
 import { AppDataSource } from '../../src/config/data-source'
 import { User } from '../../src/entity/User'
+import { Roles } from '../../src/constants'
 
 describe('POST /auth/register', () => {
     let connection: DataSource
@@ -106,15 +107,53 @@ describe('POST /auth/register', () => {
                 password: 'password',
             }
             // Act
-            const response = await request(app)
-                .post('/auth/register')
-                .send(userData)
+            await request(app).post('/auth/register').send(userData)
 
             // Assert
             const repository = connection.getRepository(User)
             const users = await repository.find()
             expect(users[0]).toHaveProperty('role')
             expect(users[0].role).toBe('customer')
+        })
+
+        it('should stored the hashed password in db', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'Prabhat',
+                lastName: 'Mishra',
+                email: 'prabhat1284@gmail.com',
+                password: 'password',
+            }
+            // Act
+            await request(app).post('/auth/register').send(userData)
+
+            // Assert
+            const repository = connection.getRepository(User)
+            const users = await repository.find()
+            expect(users[0].password).not.toBe(userData.password)
+            expect(users[0].password).toHaveLength(60)
+            expect(users[0].password).toMatch(/^\$2[a|b]\$\d+\$/)
+        })
+
+        it('should return 400 status code if email already exists', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'Prabhat',
+                lastName: 'Mishra',
+                email: 'prabhat1284@gmail.com',
+                password: 'password',
+            }
+            const userRepository = connection.getRepository(User)
+            await userRepository.save({ ...userData, role: Roles.CUSTOMER })
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData)
+
+            const users = await userRepository.find()
+            // Assert
+            expect(response.statusCode).toBe(400)
+            expect(users).toHaveLength(1)
         })
     })
 
