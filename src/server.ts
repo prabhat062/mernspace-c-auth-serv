2ac268791bc0a0ce546bd5c85b8be176532c9efa
Prabhat Mishra <prabhat1284@gmail.com>
2025-10-25 11:56:33 +0530

    add database connection in server

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 import app from './app'
 import { Config } from './config'
+import { AppDataSource } from './config/data-source'
 import logger from './config/logger'
 
-const startServer = () => {
+const startServer = async () => {
     const PORT = Config.PORT
     try {
+        await AppDataSource.initialize()
+        logger.info('Database connected succcessfully')
         app.listen(PORT, () => {
             logger.info('Server Listening on Port', { port: PORT })
         })
     } catch (error) {
-        console.log(error)
+        logger.error('Error starting server', { error })
         process.exit(1)
     }
 }
 
-startServer()
+startServer().catch((error: unknown) => {
+    logger.error('Unhandled error in startServer', { error })
+    process.exit(1)
+})
